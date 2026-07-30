a0c991b5a3b8ec7c1e876e25832b263e9f7f5570
kinnaamf <dimoje.1337@gmail.com>
2026-07-31 01:48:07 +0300

    feat: add weekly calendar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import AppHeader from "@/components/AppHeader.vue";
+import WeeklyCalendar from "@/components/WeeklyCalendar.vue";
 </script>
 
 <template>
   <div class="px-4 py-8">
     <AppHeader/>
+    <main class="mt-6">
+      <WeeklyCalendar/>
+    </main>
   </div>
 </template>
 
---
+++ src/components/WeeklyCalendar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  const now = new Date();
+
+  const todayIndex = (now.getDay() + 6) % 7;
+
+  const startOfWeek = new Date(now);
+  startOfWeek.setDate(now.getDate() - todayIndex);
+
+  const dates = Array.from({ length: 7}, (_, i) => {
+    const d = new Date(startOfWeek);
+    d.setDate(startOfWeek.getDate() + i);
+
+    return {
+      day: d.toDateString().split(' ')[0],
+      date: d.getDate()
+    }
+  });
+</script>
+
+<template>
+  <div class="p-4 border border-border bg-card rounded-2xl flex justify-between gap-2">
+    <div v-for="(date, index) in dates"
+         :key="index"
+         class="flex flex-col gap-1.5 items-center">
+      <span class="text-xs font-semibold tracking-wide text-muted-foreground uppercase">
+        {{ date.day }}
+      </span>
+      <div class="w-9 h-9 rounded-full flex items-center justify-center"
+          :class="[index == todayIndex ? 'bg-primary shadow-md shadow-primary/50' : '',
+                   index < todayIndex ? 'border border-border bg-card-foreground/5' : '']"
+      >
+        <span class="text-sm font-semibold"
+              :class="[index === todayIndex ? 'text-primary-foreground' : '',
+                      index > todayIndex ? 'text-muted-foreground' : '']"
+        >
+          {{ date.date }}
+        </span>
+      </div>
+      <div
+          v-if="index <= todayIndex"
+          class="w-1.5 h-1.5 rounded-full"
+          :class="index === todayIndex ? 'bg-primary' : 'bg-primary/40'"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
