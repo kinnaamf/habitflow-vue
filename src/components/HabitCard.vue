12e9f361c5e0c31f516b6d5a4ab9f365347bf68d
kinnaamf <dimoje.1337@gmail.com>
2026-08-03 21:39:02 +0300

    feat: add habit card component

--- src/components/HabitCard.vue
+++ src/components/HabitCard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import type { Habit } from "@/stores/habits.ts";
-import { Ellipsis } from "@lucide/vue";
-import { CheckIcon } from "@lucide/vue";
+import { Ellipsis, CheckIcon } from "@lucide/vue";
+import { ref } from "vue";
+import HabitPopover from "@/components/HabitPopover.vue";
+import { onClickOutside } from "@vueuse/core";
 
 defineProps<{
   habit: Habit
@@ -11,11 +13,17 @@ defineEmits<{
   (e: 'toggleStatus', habit: Habit): void;
 }>();
 
+const showPopover = ref<boolean>(false);
 
+const popoverRef = ref<HTMLElement | null>(null);
+
+onClickOutside(popoverRef, () => {
+  showPopover.value = false;
+});
 </script>
 
 <template>
-  <div class="flex items-center bg-accent/50 justify-between border border-border p-4 rounded-2xl transition-all duration-200"
+  <div class="flex items-center bg-accent/50 justify-between border border-border p-4 rounded-2xl transition-all duration-200 relative"
        :class="habit.status === 'completed' ? '' : 'hover:bg-accent/20 hover:border-primary/30'"
   >
     <div class="flex items-center gap-3">
@@ -43,13 +51,38 @@ defineEmits<{
         </span>
         </p>
       </div>
-      <div class="h-full py-3 pl-3 group">
-        <Ellipsis :size="20" class="stroke-primary/50 group-hover:stroke-primary transition-all duration-200" />
+      <div class="relative">
+        <div
+            class="h-full py-3 pl-3 group"
+            @click.stop="showPopover = !showPopover">
+          <Ellipsis :size="20" class="stroke-primary/50 group-hover:stroke-primary transition-all duration-200" />
+        </div>
+        <Transition name="appear">
+          <HabitPopover
+              ref="popoverRef"
+              v-if="showPopover"
+              class="absolute right-4 bottom-4"/>
+        </Transition>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="postcss">
+.appear-enter-active,
+.appear-leave-active {
+  transition: all .2s ease-out;
+}
 
+.appear-enter-from,
+.appear-leave-to {
+  opacity: 0;
+  transform: scale(95%);
+}
+
+.appear-enter-to,
+.appear-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
 </style>
